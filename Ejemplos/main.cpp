1a60d74799515b3cb9fa69affaa9dbeb3cb74a85
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-11-23 17:22:30 -0600

    Evalua un palindromo, con funcion recursiva

--- Ejemplos/main.cpp
+++ Ejemplos/main.cpp
@@ -2,25 +2,60 @@
 #include <stdlib.h>
 using namespace std;
 
-string f(int n){
-    cout<<"f("<<n<<")"<<endl;
-    if(n==0)
-        return "";
-    else if(n%2==0)
-        return f(n/2) + "0";
-    else
-        return f(n/2) + "1";
+bool esNumero(char c){
+    return 48<=c && c <=57;
+}
+bool esMayuscula(char c){
+    return 65<=c && c <=90;
+}
+bool esMinuscula(char c){
+    return 97<=c && c <=122;
+}
+bool esLetra(char c){
+    return esMayuscula(c) || esMinuscula(c);
+}
+bool esEspecial(char c){
+    return !esLetra(c) && !esNumero(c);
 }
 
-int main(){
-    int n;
+string procesaCadena(string Cadena){
+    string Procesada;
+    char c;
+    int i,tam;
+    tam = Cadena.size();
 
-    cout<<"Ingresa n ";
-    cin>>n;
-    cout<<endl<<endl;
+    for(i=0; i<tam; i++)
+        if(!esEspecial(Cadena.at(i)))
+            Procesada.push_back(toupper(Cadena.at(i)));
 
-    string binario = f(n);
-    cout<<endl<<endl<< n <<"(10) = "<< binario <<"(2)"<<endl<<endl;
+    return Procesada;
+}
+
+bool esPalindromo(string& Cadena, int ini, int fin){
+    int n = fin-ini+1;
+    if(n <= 1)
+        return true;
+    else if(Cadena[ini]!=Cadena[fin])
+        return false;
+    else{
+        cout<<Cadena.substr(ini+1, n-2)<<endl;
+        return esPalindromo(Cadena, ini+1, fin-1);
+    }
+}
+
+int main(void){
+    string Cadena, Procesada;
+    char c;
+    int i,tam;
+    cout<<"Ingresa una cadena de caracteres ";
+    getline(cin, Cadena);
+
+    Procesada = procesaCadena(Cadena);
+
+    if(esPalindromo(Procesada,0,Procesada.size()-1))
+        cout<<"ES palindromo."<<endl;
+    else
+        cout<<"NO es palindromo."<<endl;
 
     return 0;
 }
