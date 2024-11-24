ea6593f96c671a54b8111b0a1c61a0350c2b3f58
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-11-23 18:15:01 -0600

    MergeSort para vector

--- Ejemplos/main.cpp
+++ Ejemplos/main.cpp
@@ -3,69 +3,87 @@
 #include <time.h>
 using namespace std;
 
-void rellenaVector(int n, int X[50]){
-    for(int i=0; i<n; i++)
-        X[i] = rand()%100;
+void rellenaVector(int V[], int n) {
+    for (int i = 0; i < n; i++)
+        V[i] = rand()%100;
+}
+void muestraVector(int V[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << V[i] << " ";
+    cout << endl;
 }
 
-void muestraVector(int n, int X[50]){
-    for(int i=0; i<n; i++)
-        cout<<X[i]<<" ";
-    cout<<endl;
-}
+//Fusiona mitades
+void fusiona(int V[], int izq, int mit, int der) {
+    int n1 = mit - izq + 1;
+    int n2 = der - mit;
 
-void intercambia(int X[50], int i, int f){
-    int aux=X[i];
-    X[i] = X[f];
-    X[f] = aux;
-}
+    //Vectores para cada mitad
+    int Izq[n1], Der[n2];
 
-int particiona(int X[50], int ini, int fin){
-    int piv = X[fin];
-    int i=ini;
-    int f=fin-1;
-    cout<<"ini="<<ini<<" fin="<<fin<<" piv="<<piv<<endl;
-    while(i<=f){
-        if(X[i]>piv){
-            intercambia(X,i,f);
-            f--;
-        }
-        else{
+    //Se copian temporalmente los vectores
+    for (int i = 0; i < n1; i++)
+        Izq[i] = V[izq + i];
+
+    for (int j = 0; j < n2; j++)
+        Der[j] = V[mit + 1 + j];
+
+    // Se fusionan ordenadamente los arreglos Izq y Der, en V
+    int i = 0, j = 0, k = izq;
+    while (i < n1 && j < n2) {
+        if (Izq[i] <= Der[j]) {
+            V[k] = Izq[i];
             i++;
         }
+        else {
+            V[k] = Der[j];
+            j++;
+        }
+        k++;
     }
-    cout<<"i="<<i<<" f="<<f<<" piv="<<piv<<endl;
-    intercambia(X,i,fin);
-    return i-1;
-}
 
-void quickSort(int X[50], int ini, int fin){
-    if(ini<fin){
-        int p = particiona(X, ini, fin);
-        cout<<"p="<<p<<endl;
-        quickSort(X,ini,p);
-        quickSort(X,p+1,fin);
+    //Se copian los restantes de Izq
+    while (i < n1) {
+        V[k] = Izq[i];
+        i++;
+        k++;
+    }
+
+    //Se copian los restantes de Der
+    while (j < n2) {
+        V[k] = Der[j];
+        j++;
+        k++;
     }
 }
 
-void quickSort(int n, int X[50]){
-    quickSort(X,0,n-1);
+void mergeSort(int V[], int izq, int der) {
+    if (izq < der) {
+        int mit = izq + (der - izq) / 2;
+
+        //Se divide recursivamente en mitades
+        mergeSort(V, izq, mit);
+        mergeSort(V, mit + 1, der);
+
+        //Se fusiona ordenadamente las mitades
+        fusiona(V, izq, mit, der);
+    }
 }
 
-int main(void){
+int main() {
     srand(time(NULL));
-    int n, V[50];
+    int V[50];
+    int n;
+
     cout<<"Cuantos? ";cin>>n;
 
-    rellenaVector(n, V);
-    cout<<endl<<"V inicial"<<endl;
-    muestraVector(n, V);
-    cout<<endl<<endl;
+    rellenaVector(V,n);
+    cout << "V al inicio -> ";
+    muestraVector(V, n);
 
-    quickSort(n, V);
-    cout<<endl<<"V final"<<endl;
-    muestraVector(n, V);
-    cout<<endl<<endl;
+    mergeSort(V, 0, n - 1);
 
+    cout << "V al final  -> ";
+    muestraVector(V, n);
     return 0;
 }
